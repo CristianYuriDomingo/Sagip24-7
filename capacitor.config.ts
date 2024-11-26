fb9eb81df0911221ed7f4256ab8afc14786da362
unknown <dcristianyuri@gmail.com>
2024-11-26 14:48:13 +0800

    Initial push

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Sagip-24-7',
+  webDir: 'www'
+};
+
+export default config;
