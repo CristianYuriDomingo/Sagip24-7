ac745f8af0fe24bd0d122473f0302230bf9d5509
unknown <dcristianyuri@gmail.com>
2024-12-05 14:27:52 +0800

    Latest

--- src/app/visual-aids/visual-aids.page.ts
+++ src/app/visual-aids/visual-aids.page.ts
@@ -5,11 +5,59 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './visual-aids.page.html',
   styleUrls: ['./visual-aids.page.scss'],
 })
-export class VisualAidsPage implements OnInit {
+export class VisualAidsPage{
+
+  isResModalOpen = false;
+  isTraumModalOpen = false;
+  isEnvModalOpen = false;
+  isAllModalOpen = false;
+  isCardModalOpen = false;
+  isOtherModalOpen = false;
+ 
 
   constructor() { }
 
-  ngOnInit() {
+  openResModal() {
+    this.isResModalOpen = true;
   }
+  openTraumModal() {
+    this.isTraumModalOpen = true;
+  }
+  openEnvModal() {
+    this.isEnvModalOpen = true;
+  }
+  openAllModal() {
+    this.isAllModalOpen = true;
+  }
+  openCardModal() {
+    this.isCardModalOpen = true;
+  }
+  openOtherModal() {
+    this.isOtherModalOpen = true;
+  }
+  
+
+
+
+  closeResModal() {
+    this.isResModalOpen = false;
+  }
+  closeTraumModal() {
+    this.isTraumModalOpen = false;
+  }
+  closeEnvModal() {
+    this.isEnvModalOpen = false;
+  }
+  closeAllModal() {
+    this.isAllModalOpen = false;
+  }
+  closeCardModal() {
+    this.isCardModalOpen = false;
+  }
+  closeOtherModal() {
+    this.isOtherModalOpen = false;
+  }
+
+ 
 
 }
