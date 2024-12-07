5fd8f3b85b93510e4897a97e5b6a9266c7d4a22a
unknown <dcristianyuri@gmail.com>
2024-12-07 13:51:25 +0800

    malapit na talaga

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,10 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
   constructor() {}
 
+  // Method to open the dialer
+  dialEmergencyContact() {
+    window.open('tel:911', '_system'); // Opens the native dialer
+  }
 }
